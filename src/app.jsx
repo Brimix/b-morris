f62f339fe498b5eba601580bf75dde81edceb865
Brimix <only.brimix@gmail.com>
2023-07-12 11:49:09 -0300

    Initial config

---
+++ src/app.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return (
+      <div>
+          <p>Hola!</p>
+      </div>
+  );
+};
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const webpack = require('webpack');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+const APP_DIST_PATH = path.resolve(__dirname, './dist');
+
+module.exports = {
+  entry: ['babel-polyfill', './src/app'],
+  output: {
+    path: APP_DIST_PATH,
+    filename: '[name].js',
+  },
+  resolve: {
+    modules: ['./node_modules'],
+    extensions: ['.js', '.jsx'],
+  },
+  devServer: {
+    static: {
+      directory: APP_DIST_PATH,
+      staticOptions: {},
+      publicPath: '/',
+      serveIndex: true,
+      watch: true,
+    },
+    open: true,
+    host: 'localhost',
+    port: '8001',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx)$/,
+        use: ['babel-loader'],
+      }
+    ]
+  },
+  plugins: [
+    new htmlWebpackPlugin({
+      template: `./src/app.html`,
+      filename: `${APP_DIST_PATH}/index.html`,
+      // favicon: `./src`,
+    }),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+    }),
+  ],
+  mode: 'none',
+};
