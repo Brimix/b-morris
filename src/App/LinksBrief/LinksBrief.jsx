3dee31684307f93471a449cc5c564421e3c914f6
Brimix <only.brimix@gmail.com>
2023-07-14 18:29:01 -0300

    Add Linkedin row

--- src/App/LinksBrief/LinksBrief.jsx
+++ src/App/LinksBrief/LinksBrief.jsx
@@ -5,6 +5,10 @@ import './LinksBrief.scss';
 const LinksBrief = () => {
   const onGithubClick = useCallback(() => window.open("https://github.com/Brimix", "_blank"), []);
 
+  const onLinkedinClick = useCallback(() => window.open("https://www.linkedin.com/in/brian-morris-esquivel/", "_blank"), []);
+
+  const onResumeClick = useCallback(() => window.open(resumePdf, "_blank"), []);
+
   return (
     <div className="links-brief">
       <p> Site in development... </p>
@@ -13,7 +17,12 @@ const LinksBrief = () => {
         <button onClick={onGithubClick}> Github </button>
       </div>
       <div>
-        <label> Download my resume </label>
+        <label> Stalk me on Linkedin </label>
+        <button onClick={onLinkedinClick}> Linkedin </button>
+      </div>
+      <div>
+        <label> Check my resume! </label>
+        <button onClick={onResumeClick}> Open </button>
         <a href={resumePdf} Download="Resume - Brian Morris">
           <button type = "button"> Download </button>
         </a>
