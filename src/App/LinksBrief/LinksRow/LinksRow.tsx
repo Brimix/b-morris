4181c0b22ec3f9e972ba4a08d838322e5f75c656
Brian Morris <31920289+Brimix@users.noreply.github.com>
2023-07-19 00:09:25 -0300

    ENH - Upgrade buttons style (#5)

--- src/App/LinksBrief/LinksRow/LinksRow.tsx
+++ src/App/LinksBrief/LinksRow/LinksRow.tsx
@@ -1,8 +1,7 @@
-import {useCallback, useMemo} from 'react';
+import {SyntheticEvent, useCallback, useMemo} from 'react';
 import {ReactSVG} from 'react-svg';
 import './LinksRow.scss';
 
-import iconShare from '../../../../img/iconmonstr-share-11.svg';
 import iconDownload from '../../../../img/iconmonstr-download-4.svg';
 
 export interface LinksRowProps {
@@ -18,26 +17,22 @@ const LinksRow = (props: LinksRowProps) => {
 
   const isDownloadable = useMemo(() => downloadableFilename !== undefined, [downloadableFilename]);
 
-  const onClickOpen = useCallback(() => window.open(url, "_blank"), [url]);
+  const onClick = useCallback(() => window.open(url, "_blank"), [url]);
+  const onClickDownload = useCallback((event: SyntheticEvent) => event.stopPropagation(), []);
 
   return (
-    <div className={`links-row links-row-color--${color}`}>
+    <div className={`links-row links-row-color--${color}`} onClick={onClick}>
       <div className="links-row__label">
         <ReactSVG className="links-row__icon" src={icon}/>
         <label> {label} </label>
       </div>
-      <div>
-        <button onClick={onClickOpen}>
-          <img src={iconShare} alt="Go" />
-        </button>
-        {isDownloadable && (
-          <a href={url} download={downloadableFilename}>
-            <button>
-              <img src={iconDownload} alt="Download" />
-            </button>
+      {isDownloadable && (
+        <div className={`links-row__button links-row-color--${color}`}>
+          <a href={url} download={downloadableFilename} onClick={onClickDownload}>
+            <ReactSVG src={iconDownload}/>
           </a>
-        )}
-      </div>
+        </div>
+      )}
     </div>
   )
 };
